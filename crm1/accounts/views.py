7647c5eadf5b3e5ff14ee1924c27c77e1df5d697
Aksaule <aksaule124026@gmail.com>
2021-07-12 18:20:21 +0600

    Last changes

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -2,10 +2,22 @@ from django.db.models.query import InstanceCheckMeta
 from django.shortcuts import render, redirect
 from .models import *
 from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
 from .forms import OrderForm
 from .filters import OrderFilter
 
 # Create your views here.
+def registerPage(request):
+    form = UserCreationForm()
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+
+def loginPage(request):
+    context = {}
+    return render(request, 'accounts/login.html', context)
+
+
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
