e1eb26add1b870150939cae52bfe61a732bf0938
Aksaule <aksaule124026@gmail.com>
2021-07-21 12:40:44 +0600

    End video 17

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import Group
 
 # Create your views here.
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from .filters import OrderFilter
 from .decorators import unauthenticated_user, allowed_users, admin_only
 
@@ -160,3 +160,18 @@ def userPage(request):
                'total_orders':total_orders, 'pending':pending}
 
     return render(request, 'accounts/products.html', context)
+
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+
+    customer = request.user.customer
+    form = CustomerForm(instance=customer)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=customer)
+        if form.is_valid():
+            form.save()
+    context={'form':form}
+    return render(request, 'accounts/account_settings.html', context)
