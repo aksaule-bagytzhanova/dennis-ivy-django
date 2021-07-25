59374370084a9cdff245634d92e21790499b82fe
Aksaule <aksaule124026@gmail.com>
2021-07-25 15:05:21 +0600

    finish 21 video

--- crm1/crm1/settings.py
+++ crm1/crm1/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'crm1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER': 'postgres',
+        'PASSWORD': 'aksiaksi0012041199A',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
